9d89fe240db35de15b34ed29f50ce31c05e5cfd4
Steven-Roberts <steven94@vt.edu>
2016-09-20 18:19:35 -0400

    Initial code commit

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+    parserOptions: {ecmaVersion: 7},
+    extends: 'eslint:all',
+    rules: {
+        'no-console': 'off',
+        'no-constant-condition': 'warn',
+        'no-debugger': 'warn',
+        'no-empty': 'warn',
+        'no-template-curly-in-string': 'warn',
+        'no-unreachable': 'warn',
+        'dot-location': ['error', 'property'],
+        'no-alert': 'warn',
+        'no-warning-comments': 'warn',
+        'linebreak-style': 'off',
+        'sort-keys': 'off',
+        'sort-vars': 'off',
+        'quote-props': ['error', 'consistent-as-needed'],
+        'padded-blocks': ['error', {
+            blocks: 'never',
+            classes: 'never',
+            switches: 'never'
+        }],
+        'quotes': ['error', 'single'],
+        'multiline-ternary': 'off',
+        'no-ternary': 'off',
+        'one-var': ['error', {
+            initialized: 'never',
+            uninitialized: 'always'
+        }],
+        'space-before-function-paren': ['error', 'never'],
+        'func-style': 'off',
+        'max-params': 'off',
+        'require-jsdoc': ['warn', {
+            require: {
+                FunctionDeclaration: true,
+                MethodDefinition: true,
+                ClassDeclaration: true
+            }
+        }],
+        'newline-before-return': 'off',
+        'id-length': 'off'
+    }
+};
