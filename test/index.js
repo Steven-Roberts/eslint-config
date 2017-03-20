949f591cfc943f0209a88e69eab645f231266314
Steven Roberts <sroberts994@gmail.com>
2017-03-19 23:55:43 -0400

    Add simple js file for testing eslint rules

---
+++ test/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const logger = require('logger');
+
+/**
+ * Class representing a person
+ */
+class Person {
+
+    /**
+     * Constructs a person with a name
+     * @param {string} firstName - The first name of the person
+     * @param {string} lastName - The last name of the person
+     */
+    constructor (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Sets the age of the person in years
+     * @param {number} age - The age in years
+     */
+    setAge (age) {
+        this.age = age;
+    }
+
+    /**
+     * Gets the full name of the person
+     * @returns {string} The full name
+     */
+    get fullName () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+}
+
+const person = new Person('Bob', 'McDonald');
+
+let a = Math.random();
+
+a += Date.now();
+
+person.setAge(a);
+
+if (a > Math.PI) {
+    const obj = {
+        a: 'hi',
+        b: [Math.E, Math.cos(1), null]
+    };
+
+    logger.log(obj);
+}
+
+module.exports = {
+    person
+};
